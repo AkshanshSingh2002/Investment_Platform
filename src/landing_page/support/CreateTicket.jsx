dac329f3ca6a78bf92ecfc89d94379403c5cda71
Akshansh Singh <akshanshs2002@gmail.com>
2025-06-06 21:37:20 +0530

    support-component

--- src/landing_page/support/CreateTicket.jsx
+++ src/landing_page/support/CreateTicket.jsx
@@ -2,7 +2,60 @@ import React from 'react';
 
 function CreateTicket() {
     return ( 
-        <h1>CreateTicket</h1>
+        <div className="container p-5 mb-5">
+      <div className="row mt-5 p-5">
+        <h2 className="mb-5 text-muted">To create a ticket, select a relevant topic</h2>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h3><i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening</h3>
+          <a href="#" style={{lineHeight:"2.5"}}>Resident individual</a><br />
+          <a href="#" style={{lineHeight:"2.5"}}>Minor</a><br />
+          <a href="#" style={{lineHeight:"2.5"}}>Non Resident Indian (NRI)</a><br />
+          <a href="#" style={{lineHeight:"2.5"}}>Company, Partnership, HUF and LLP</a><br />
+          <a href="#" style={{lineHeight:"2.5"}}>Glossary</a><br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+            <h3><i class="fa fa-user-o" aria-hidden="true"></i> Your Zerodha Account</h3>
+            <a href="#" style={{lineHeight:"2.5"}}>Your Profile</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Account modification</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Client Master Report (CMR) and Depository Participant (DP)</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Nomination</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Transfer and conversion of securities</a><br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+            <h3> Kite</h3>
+            <a href="#" style={{lineHeight:"2.5"}}>IPO</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Trading FAQs</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Margin Trading Facility (MTF) and Margins</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Charts and orders</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Alerts and Nudges</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>General</a><br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h3><i class="fa fa-money" aria-hidden="true"></i> Funds</h3>
+          <a href="#" style={{lineHeight:"2.5"}}>Add money</a><br />
+          <a href="#" style={{lineHeight:"2.5"}}>Withdraw money</a><br />
+          <a href="#" style={{lineHeight:"2.5"}}>Add bank accounts</a><br />
+          <a href="#" style={{lineHeight:"2.5"}}>eMandates</a><br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+            <h3><i class="fa fa-terminal" aria-hidden="true"></i>  Console</h3>
+            <a href="#" style={{lineHeight:"2.5"}}>Portfolio</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Corporate actions</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Funds statement</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Reports</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Profile</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Segments</a><br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+            <h3><i class="fa fa-usd" aria-hidden="true"></i> Coin</h3>
+            <a href="#" style={{lineHeight:"2.5"}}>Mutual funds</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>National Pension Scheme (NPS)</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Features on Coin</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>Payments and Orders</a><br />
+            <a href="#" style={{lineHeight:"2.5"}}>General</a><br />
+        </div>
+      </div>
+    </div>
      );
 }
 
--- src/landing_page/support/Hero.jsx
+++ src/landing_page/support/Hero.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <section className="container-fluid" id="supportHero">
+        <div className="p-5" id="supportWrapper">
+            <h3>Support Portal</h3>
+            <h3 style={{textDecoration:"underline"}}><a href="#">Track tickets</a></h3>   
+        </div>
+        <div className="row m-2">
+            <div className="col-6 p-5">
+                <h2>Search for an answer or browse help topics to create a ticket</h2>
+                <input type="text" placeholder="How do i activate F&O"/><br />
+                
+                <a href="#">Track account opening</a> 
+                <a href="#">Track segment activation</a> 
+                <a href="#">Intraday margins </a>
+                <a href="#">Kite user manual</a>
+                
+            </div>
+            <div className="col-6 p-5">
+                <h2>Featured</h2>
+                <ol>
+                    <li>
+                        <a href="#">Surveillance measure on scrips - June 2025</a>
+                    </li>
+                    <li>
+                        <a href="#">Offer for sale (OFS) – June 2025</a>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </section>
+  );
 }
 
 export default Hero;
